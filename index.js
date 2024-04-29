35b9c464af08e2291a16d2a1d3e636e665dfc5f2
aashishrl <iashish9800@gmail.com>
2024-04-29 12:18:32 +0545

    merging data in rough

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 const { moreInformation } = require('./moreInformation');
 const { manageOptions } = require('./manageOptions');
+const { supplierPreference } = require('./rough');
 
 async function scrapeAndTranslate() {
     const browser = await puppeteer.launch();
@@ -10,8 +11,9 @@ async function scrapeAndTranslate() {
 
     const moreInfoData = await moreInformation(page);
     const manageOptionsData = await manageOptions(page);
+    const supplierPreferenceData = await supplierPreference(page);
 
-    const scrapedTitle = await page.evaluate((moreInfoData, manageOptionsData) => {
+    const scrapedTitle = await page.evaluate((moreInfoData, manageOptionsData,supplierPreferenceData) => {
         const title = document.querySelector('.fc-dialog-headline').innerText;
 
         const permissionElements = document.querySelectorAll('.fc-stacks.fc-dialog-restricted-content ul li .fc-stack-name');
@@ -26,6 +28,8 @@ async function scrapeAndTranslate() {
         const buttons = Array.from(buttonElements, element => element.innerText.trim());
 
         const manageOptionsButton = manageOptionsData; 
+
+        const supplierPreferencePage = supplierPreferenceData; 
         
         return {
             cookieBanner:{
@@ -35,9 +39,10 @@ async function scrapeAndTranslate() {
                 moreInfo: moreInfo,
                 buttons: buttons,
                 manageOptionsButton: manageOptionsButton,
+                supplierPreferencePage: supplierPreferencePage,
             }
         };
-    }, moreInfoData, manageOptionsData);
+    }, moreInfoData, manageOptionsData, supplierPreferenceData);
 
 
     fs.writeFileSync('weeronline_webscraping.json', JSON.stringify(scrapedTitle, null, 2));
--- manageOptions.js
+++ manageOptions.js
@@ -1,10 +1,55 @@
-async function manageOptions(page) {
-    await page.click('.fc-button.fc-cta-manage-options');
-    await page.waitForSelector('.fc-data-preferences-dialog');
+// const { supplierPreference } = require('./rough');
 
-    const data = await page.evaluate(() => {
+// async function manageOptions(page) {
+//     // await page.click('.fc-button.fc-cta-manage-options');
+//     // await page.waitForSelector('.fc-data-preferences-dialog');
+
+
+
+//     const data = await page.evaluate(() => {
+//         const manageOptTitle = document.querySelectorAll('.fc-header.fc-dialog-restricted-content h1')[1].innerText;
+//         const manageOptSubTitle = document.querySelectorAll('.fc-body.fc-dialog-restricted-content p')[0].innerText;
+
+//         const preferencesContainer = document.querySelector('.fc-preferences-container');
+//         const preferenceList = Array.from(preferencesContainer.querySelectorAll('.fc-preference-container'), (e) => {
+//             const title = e.querySelector('.fc-preference-title h2').innerText;
+//             const description = e.querySelector('.fc-preference-description .fc-purpose-feature-description').innerText;
+//             const consent = e.querySelector('.fc-consent-preference-container');
+//             const legitimateInterest = e.querySelector('.fc-legitimate-interest-preference-container');
+
+//             return {
+//                 title: title,
+//                 description: description,
+//                 consent: consent ? consent.innerText : '',
+//                 legitimateInterest: legitimateInterest ? legitimateInterest.innerText : '',
+//             };
+//         });
+
+//         return {
+//             title: manageOptTitle,
+//             subTitle: manageOptSubTitle,
+//             preferenceList: preferenceList
+//         };
+//     });
+
+//     return data;
+// }
+
+// module.exports = { manageOptions };
+
+const { supplierPreference } = require('./rough');
+
+async function manageOptions(page) {
+    // await page.click('.fc-button.fc-cta-manage-options');
+    // await page.waitForSelector('.fc-data-preferences-dialog');
+
+    const supplierPreferenceData = await supplierPreference(page);    
+
+    const data = await page.evaluate((supplierPreferenceData) => {
         const manageOptTitle = document.querySelectorAll('.fc-header.fc-dialog-restricted-content h1')[1].innerText;
         const manageOptSubTitle = document.querySelectorAll('.fc-body.fc-dialog-restricted-content p')[0].innerText;
+
+        const supplierPreferencePage = supplierPreferenceData; 
 
         const preferencesContainer = document.querySelector('.fc-preferences-container');
         const preferenceList = Array.from(preferencesContainer.querySelectorAll('.fc-preference-container'), (e) => {
@@ -24,9 +69,11 @@ async function manageOptions(page) {
         return {
             title: manageOptTitle,
             subTitle: manageOptSubTitle,
-            preferenceList: preferenceList
+            preferenceList: preferenceList,
+            supplierPreferencePage: supplierPreferencePage,
+
         };
-    });
+    },supplierPreferenceData);
 
     return data;
 }
